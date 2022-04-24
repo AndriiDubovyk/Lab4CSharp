451c1985a27ddf3a459080ee4c770142a69a6901
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-04-24 14:00:12 +0300

    Add UserCandidate class

---
+++ Lab4CSharp/Models/UserCandidate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4CSharp.Models
+{
+    class UserCandidate
+    {
+        #region Fields
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private DateTime? _birthdate;
+        #endregion
+
+        #region Properties
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+        public DateTime? Birthdate
+        {
+            get
+            {
+                return _birthdate;
+            }
+            set
+            {
+                _birthdate = value;
+            }
+        }
+        #endregion
+    }
+}
