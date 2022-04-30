c42808305e0f399ca2f681b1143002e60d94161d
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-04-30 20:51:42 +0300

    Implement user adding

--- Lab4CSharp/ViewModels/UserTableViewModel.cs
+++ Lab4CSharp/ViewModels/UserTableViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Lab4CSharp.ViewModels
 {
@@ -14,6 +15,8 @@ namespace Lab4CSharp.ViewModels
         #region Fields
         private ObservableCollection<User> _users;
         private User _selectedUser;
+        private UserCandidate _userAdd = new UserCandidate();
+        private RelayCommand<object> _addCommand;
         private RelayCommand<object> _removeCommand;
         #endregion
 
@@ -43,11 +46,67 @@ namespace Lab4CSharp.ViewModels
 
         }
 
+        public string FirstNameAdd
+        {
+            get
+            {
+                return _userAdd.FirstName;
+            }
+            set
+            {
+                _userAdd.FirstName = value;
+            }
+        }
+
+        public string LastNameAdd
+        {
+            get
+            {
+                return _userAdd.LastName;
+            }
+            set
+            {
+                _userAdd.LastName = value;
+            }
+        }
+
+        public string EmailAdd
+        {
+            get
+            {
+                return _userAdd.Email;
+            }
+            set
+            {
+                _userAdd.Email = value;
+            }
+        }
+
+        public DateTime? BirthdateAdd
+        {
+            get
+            {
+                return _userAdd.Birthdate;
+            }
+            set
+            {
+                _userAdd.Birthdate = value;
+            }
+        }
+
         public RelayCommand<object> RemoveCommand
         {
             get
             {
                 return _removeCommand ??= new RelayCommand<object>(_ => Remove(), CanExecuteRemoveCommand);
+            }
+        }
+
+        public RelayCommand<object> AddCommand
+        {
+            get
+            {
+                return _removeCommand ??= new RelayCommand<object>(_ => Add(), CanExecuteAddCommand);
             }
         }
         #endregion
@@ -80,9 +139,29 @@ namespace Lab4CSharp.ViewModels
             Users.Remove(SelectedUser);
         }
 
+        private void Add()
+        {
+           try
+            {
+                Users.Add(new User(FirstNameAdd, LastNameAdd, EmailAdd, BirthdateAdd.Value));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Process failed: {ex.Message}");
+            }
+        }
+
         private bool CanExecuteRemoveCommand(object obj)
         {
             return SelectedUser!=null;
+        }
+
+        private bool CanExecuteAddCommand(object obj)
+        {
+            return !String.IsNullOrWhiteSpace(FirstNameAdd)
+                && !String.IsNullOrWhiteSpace(LastNameAdd)
+                && !String.IsNullOrWhiteSpace(EmailAdd)
+                && BirthdateAdd != null;
         }
 
     }
